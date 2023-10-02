b073cc229f5a18189cb19ed013b311a1faaf6909
Eugene-Kabeyi <eugenekabeyi144@gmail.com>
2023-10-02 11:58:15 +0300

    Practice python

---
+++ app.py
@@ -0,0 +1,5 @@
+#A program used to display the color someone lo=ike after input
+name = input("What is your name?\t")
+color = input("What is your favourite color?\t")
+
+print("Hello "+ name + ", your favourite color is "+color)
