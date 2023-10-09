ea6071f1bd0234b394dc37e96c4b6c554d806219
Eugene-Kabeyi <eugenekabeyi144@gmail.com>
2023-10-09 09:56:30 +0300

    String practice

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 #A program used to display the color someone lo=ike after input
-name = input("What is your name?\t")
-color = input("What is your favourite color?\t")
-
-print("Hello "+ name + ", your favourite color is "+color)
+# name = input("What is your name?\t")
+# color = input("What is your favourite color?\t")
+#
+# print("Hello "+ name + ", your favourite color is "+color)
+#calculate age
+birthDate = int(input("Enter your year of Birth:\t"))
+age = 2023 - birthDate
+print(age)
---
+++ str.py
@@ -0,0 +1,10 @@
+#all about strings
+emails = '''
+Hello John,
+
+We are glad to have you i our team as we get ready for the upcoming project
+Regards,
+Eugene Kabeyi
+C.E.O White Deco LTD.
+'''
+print(emails)
