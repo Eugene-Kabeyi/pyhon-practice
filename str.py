739e357dfa7f33d086e9d3be08ff52c7f1dd12df
Eugene-Kabeyi <eugenekabeyi144@gmail.com>
2023-10-09 20:37:36 +0300

    Still on strings

--- str.py
+++ str.py
@@ -8,3 +8,17 @@ Eugene Kabeyi
 C.E.O White Deco LTD.
 '''
 print(emails)
+
+book = "Think and Grow Rich"
+print(book[-9:15])
+
+first= "John"
+last= "Doe"
+msg = f'{first} {last} is a good person\n'
+print(msg)
+
+print(len(msg))
+print(msg.upper())
+print(msg.find("o"))
+print(msg.replace('good person','fantastic individual\n'))
+print('good' in msg)
